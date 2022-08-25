7537d1a89c2ea5848ecc8d890091d22ff75ba9bb
Priyanka-Shahi <shahipriyan@gmail.com>
2022-08-25 17:49:07 +1000

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
@@ -7,7 +7,9 @@ import About from "./About";
 import user from "../data/user";
 console.log(user);
 
-function App() {
+class App extends React.Component {
+  
+  render() {
   return (
     <div>
       <NavBar />
@@ -16,5 +18,5 @@ function App() {
     </div>
   );
 }
-
+};
 export default App;
